f4742febfd937010172a91f22d09703281416a21
jcten <46980783+jcten@users.noreply.github.com>
2019-02-05 23:34:16 +0100

    Prueba Colisión

---
+++ ColisionPlayer.cc
@@ -0,0 +1,28 @@
+int CollisionPlayer(Tplayer player, Tmapa mapa){
+  //De abajo a arriba
+  if(player.posy<mapa.posy+esat::SpriteHeight(mapa.platform_sprites)&&
+  player.posy>mapa.posy&&
+  player.posx+esat::SpriteWidth(player.player_sprites)>=mapa.posx&&
+  player.posx<=mapa.posx+esat::SpriteWidth(mapa.platform_sprites)){
+    return 1;
+  }
+  //De arriba a abajo
+  else if(player.posy+esat::SpriteHeight(mapa.player_sprites)>mapa.posy&&
+  player.posy+esat::SpriteHeight(mapa.player_sprites)<mapa.posy+esat::SpriteHeight(mapa.platform_sprites)&&
+  player.posx+esat::SpriteWidth(player.player_sprites)>=mapa.posx&&
+  player.posx+esat::SpriteWidth(player.player_sprites)<=mapa.posx+esat::SpriteWidth(mapa.platform_sprites)){
+    return 2;
+  }
+  //De derecha a Izq
+  /*else if(player.posy<mapa.posy+esat::SpriteHeight(mapa.platform_sprites)&&player.posy>mapa.posy&&player.posx>=mapa.posx&&
+  player.posx+esat::SpriteWidth(player.player_sprites)<=mapa.posx+esat::SpriteWidth(mapa.platform_sprites)){
+    return 3;
+  }
+  //De Izq a derecha
+  else if(player.posy<mapa.posy+esat::SpriteHeight(mapa.platform_sprites)&&player.posy>mapa.posy&&player.posx>=mapa.posx&&
+  player.posx+esat::SpriteWidth(player.player_sprites)<=mapa.posx+esat::SpriteWidth(mapa.platform_sprites)){
+    return 4;
+  }*/
+  //No colisiona
+  else{return 0;}
+}
