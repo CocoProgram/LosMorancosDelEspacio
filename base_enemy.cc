671abe827459160bd94a67f65e1eeb6968beb70f
chunfly <35737020+chunfly@users.noreply.github.com>
2019-02-11 19:17:05 +0100

    Update base_enemy.cc

--- base_enemy.cc
+++ base_enemy.cc
@@ -1,20 +1,4 @@
 
-esat::SpriteHandle *Block_sprite_Enemy;
-
-struct TEnemy {
-	float posx, posy;
-	esat::SpriteHandle *enemy_sprites;
-	//EnemyTypes enemy_type;
-	bool is_alive;
-	float speedx, speedy;
-	int points;
-	char colour;	
-	bool phase_animation;	
-	int seg_counter = 0;	
-};
-TEnemy *str_enemy;
-
-
 //LLamar dentro de PreMemorySaved
 void ReservaMemoriaEnemy(){
 	
