23acbb33ed5e9324547ed4fb420bd7d4df8478e1
gonzalezher <47396608+gonzalezher@users.noreply.github.com>
2019-02-12 19:58:23 +0100

    Add files via upload

--- meteoros.cc
+++ meteoros.cc
@@ -1,129 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-#include <esat/window.h>
-#include <esat/draw.h>
-#include <esat/input.h>
-#include <esat/sprite.h>
-#include <esat/time.h>
-
-#include <conio.h>
-#include <time.h>
-
-unsigned char fps = 60; //fps que trascurren en un segundo.
-double current_time,last_time;
-unsigned char fps_counter = 0; //cuenta los fps que trascurren en un segundo.
-
-const int kWindowX = 768, kWindowY = 576;
-esat::SpriteHandle *Block_sprite_Enemy;
-
-enum GamePhase { kGamePhase_Menu = 0,
-                 kGamePhase_InGame,
-                 kGamePhase_EndGame };
-
-enum EnemyTypes { kEnemyTypes_0 = 0,
-                  kEnemyTypes_1,
-                  kEnemyTypes_2,
-                  kEnemyTypes_3,
-                  kEnemyTypes_4,
-                  kEnemyTypes_5,
-                  kEnemyTypes_6,
-                  kEnemyTypes_7 };
-
-enum NaveType { kNaveType_0 = 0,
-                kNaveType_1,
-                kNaveType_2,
-                kNaveType_3 };
-
-enum BonusType { kBonusType_0 = 0,
-                 kBonusType_1,
-                 kBonusType_2,
-                 kBonusType_3 };
-
-
-bool g_right, g_left, g_jetpac, g_shoot;
-float g_gravity;
-
-struct TMapa{
-  int posx, posy;
-  esat::SpriteHandle platform_sprites;
-};
-TMapa *str_mapa;
-
-#include "platforms.cc"
-
-struct TPlayer{
-  float posx, posy;
-  esat::SpriteHandle *player_sprites; //Pasar a puntero, son 16
-  bool is_flying, is_alive;
-  bool shoot;
-  char vida = 5;
-  float jetpac;
-  double score;
-  int phase_animation;
-  float speed_walk;
-};
-TPlayer str_player;
-
-struct TShoot {
-  float posx, posy;
-  //Metodo de dibujado desconocido.
- float distance_max;
- bool collision;
-};
-TShoot *str_shoot;
-
-struct TEnemy {
-	float posx, posy;
-	esat::SpriteHandle *enemy_sprites;
-	EnemyTypes enemy_type;
-	bool is_alive;
-	float speedx, speedy;
-	int direccionx;
-	int direcciony;
-	int points;
-	char colour;
-	bool phase_animation;
-	int seg_counter = 0;
-};
-TEnemy *str_enemy;
-
-#include "base_enemy.cc"
-
-struct TBonus {
-  float posx, posy;
-  esat::SpriteHandle bonus_sprite;
-  int points;
-  BonusType bonus_type;
-  bool is_alive;
-};
-TBonus str_bonus;
-
-struct TNave {
-  float posx, posy;
-  esat::SpriteHandle nave_sprites; //puntero con numero de piezas
-  int fuel_level;
-  NaveType nave_type;
-};
-TNave *str_nave;
-
-void TimeInFps(){
-	/* Contamos los fps que trascurren y cuando llegan al máximo,
-	60, se resetea a 0, servirá para hacer transiciones entre animaciones.*/
-	//
-	if(fps_counter == fps){fps_counter = 0;}else{fps_counter++;}
-	//
-}
-
-void BoleanasTeclas(){ //EN ESTE VOID LLAMAMOS A LAS BOOLEANAS QUE INDICAN LA ACTIVACIÓN DE LAS TECLAS
-  esat::IsSpecialKeyPressed(esat::kSpecialKey_Left) != NULL ? g_left = true : g_left = false;
-  esat::IsSpecialKeyPressed(esat::kSpecialKey_Right) != NULL ? g_right = true : g_right = false;
-  esat::IsSpecialKeyPressed(esat::kSpecialKey_Space) != NULL ? g_jetpac = true : g_jetpac = false;
-  esat::IsKeyPressed('D') != NULL ? g_shoot = true : g_shoot = false;
-}
 
 
 //INICIALIZA ENEMIGO METEORO
+//Llamar antes del bucle de juego
 
 void InicializarMeteoro(TEnemy *meteoro, esat::SpriteHandle *sprite) {
 	
@@ -170,6 +49,7 @@ void InicializarMeteoro(TEnemy *meteoro, esat::SpriteHandle *sprite) {
 
 
 //MOVIMIENTO Y ANIMACION ENEMIGO METEORO
+//Llamar dentro de la funcion de dibujado
 
 void MovimientoMeteoro(TEnemy *meteoro, esat::SpriteHandle *sprite) {
 	
@@ -235,64 +115,6 @@ void MovimientoMeteoro(TEnemy *meteoro, esat::SpriteHandle *sprite) {
 
 
 
-void PreMemorySaved(){
-	
-	if((Block_sprite_Enemy = (esat::SpriteHandle *)calloc(16,sizeof(esat::SpriteHandle)))==NULL){
-				printf("ERROR");
-	}else{
-		
-	}
-	
-	if((str_enemy = (TEnemy *)calloc(4,sizeof(TEnemy)))==NULL){
-				printf("ERROR");
-	}else{
-		
-		/*InicializarBola(str_enemy, Block_sprite_Enemy);*/
-		InicializarMeteoro(str_enemy, Block_sprite_Enemy);
-	}
-	
-	PlatformsMemoryReserved();
-	
-}   //RESERVAR AQUÍ MEMORIA DE PUNTEROS AMORES
-void FreeMemorySaved(){
-	
-	
-	LiberarEnemy();
-	PlatformsFreeMemory();
-}  //LIBERAR MEMORIA DE PUNTEROS AQUÍ JOSPDEPUTAS.
-
-void LoadSprites() {
-	PlaformsLoadSprites();
-}   //CARGAR AQUÍ SPRITES CHICAS
-void SpritesRelease() {
-	
-	
-	
-	
-}  //LIBERAR AQUÍ LOS SPRITES OSTIA
-void DrawingSprites(){
-	
-	for(int i=0;i<4;i++){
-		
-		/*MovimientoBola((str_enemy+i));*/
-		/*esat::DrawSprite((*((*(str_enemy+i)).enemy_sprites + (*(str_enemy)).phase_animation)), ((*(str_enemy+i)).posx), ((*(str_enemy+i)).posy));*/
-		MovimientoMeteoro((str_enemy+i), (Block_sprite_Enemy+i));
-		
-		if((*(str_enemy+i)).is_alive){
-			esat::DrawSprite((*((*(str_enemy+i)).enemy_sprites + (*(str_enemy+i)).phase_animation)), ((*(str_enemy+i)).posx), ((*(str_enemy+i)).posy));
-		}
-	}
-	
-	PlatformsDraw();
-	// (*(str_enemy+i)).(*(
-	
-}  //VAMOH A DIBUJAR
-
-void InitializeParametres(){
-
-
-
-} //INICIAR TODAS LAS VARIABLES AQUÍ ANTES DEL BUCLE WHILE
 
 
 
@@ -310,41 +132,4 @@ void InitializeParametres(){
 
 
 
-int esat::main(int argc, char **argv) {
-	
-  srand(time(NULL));
-	
-	esat::WindowInit(kWindowX,kWindowY);
-	WindowSetMouseVisibility(true);
-	
-	PreMemorySaved();
-  
-	InitializeParametres();
-	PlatformPositions();
-	LoadSprites();
-	
-    while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
 
-    	last_time = esat::Time();
-    	esat::DrawBegin();
-    	esat::DrawClear(0,0,0);
-			TimeInFps();
-
-			//BoleanasTeclas();
-			DrawingSprites();
-			
-    	esat::DrawEnd();
-
-    	//Control fps
-    	do{
-    		current_time = esat::Time();
-    	}while((current_time-last_time)<=1000.0/fps);
-    	esat::WindowFrame();
-  }
-  FreeMemorySaved();
-  SpritesRelease();
-
-  esat::WindowDestroy();
-
-  return 0;
-}
