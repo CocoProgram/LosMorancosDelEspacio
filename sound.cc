d3f753738de9c1485a19492662cd43318c9fa7d0
CocoProgram <47332075+CocoProgram@users.noreply.github.com>
2019-02-13 19:08:38 +0100

    Update sound.cc

--- sound.cc
+++ sound.cc
@@ -1,31 +1,29 @@
 void MemoriaAudio(){
-audio_disparo.load("./resources/sounds/Disparo.wav");
-audio_nave.load("./resources/sounds/NaveParriba.wav");
-audio_intro.load("./resources/sounds/NaveParriba.wav");
+audio_disparo.load("./resources/sounds/Disparo.ogg");
+audio_nave.load("./resources/sounds/NaveParriba.ogg");
+audio_intro.load("./resources/sounds/intro.ogg");
 
 }
 
 
 
 void MenuBackgroundSound(){
-canal.play(audio_intro,100.0f);
-  /*if(canal.play(audio_intro,100.0f)){
-    printf("%f", getVolume(canal.play(audio_intro,100.0f)));
-  }*/
+canal.play(audio_intro,1000.0f);
+
 
 }
 
 
 void PlaySound(){
-  if (g_shoot == true) {
+ /* if (g_shoot == true) {
     canal.play(audio_disparo);
   } else {
   (audio_disparo).stop();
-}
+}*/
 
-  if (str_player.show_player == true) {
+  if (str_player.show_player == false) {
     canal.play(audio_nave);
-  } else if(str_player.show_player == false){
+  } else {
   (audio_nave).stop();
   }
 }
