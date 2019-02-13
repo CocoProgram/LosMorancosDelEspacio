77d221c7d573b61e56112f7845ef51c2cfd9bebc
badwaz <alexcalabuig@gmail.com>
2019-02-13 01:13:50 +0100

    Update controlEnemy.cc

--- controlEnemy.cc
+++ controlEnemy.cc
@@ -1,10 +1,8 @@
-
-
 //Llamar antes del comienzo de cada nivel
 void InicializarEnemyLevel(){
-	
+
 	switch(g_level){
-		
+
 		case 1: break;
 		case 2: InicializarPelusa(str_enemy, Block_sprite_Enemy); break;
 		case 3: InicializarBola(str_enemy, Block_sprite_Enemy); break;
@@ -13,17 +11,18 @@ void InicializarEnemyLevel(){
 		case 6: break;
 		case 7: break;
 		case 8: break;
-		default: printf("ERROR AL INICIAR ENEMIGOS-g_level = %d", g_level);		
+		default: printf("ERROR AL INICIAR ENEMIGOS-g_level = %d", g_level);
 	}
-	
+
 }
 
 
 //Llamar dentro del bucle del juego
 void ControlEnemyLevel(){
-	
+if(loadingLevel == false)
+{
 	switch(g_level){
-		
+
 		case 1: break;
 		case 2: controlPelusa(); break;
 		case 3: controlBola(); break;
@@ -32,14 +31,12 @@ void ControlEnemyLevel(){
 		case 6: break;
 		case 7: break;
 		case 8: break;
-		default: printf("ERROR AL INICIAR ENEMIGOS-g_level = %d", g_level);		
+		default: printf("ERROR AL INICIAR ENEMIGOS-g_level = %d", g_level);
 	}
-	
 }
 
-
+}
 
 //Bola al llenar la mitad de la nave, enemy++
 //cruz al completar la nave, enemy++
 //Esto solo ocurre una vez
-
