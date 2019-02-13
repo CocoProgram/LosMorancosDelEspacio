f9bcd03be57cab9aae0c870f21494ba39b61a2e4
gonzalezher <47396608+gonzalezher@users.noreply.github.com>
2019-02-13 20:10:24 +0100

    Update controlEnemy.cc

--- controlEnemy.cc
+++ controlEnemy.cc
@@ -10,7 +10,7 @@ void InicializarEnemyLevel(){
 		case 3: InicializarBola(str_enemy, Block_sprite_Enemy); break;
 		case 4: InicializarAvion(str_enemy, Block_sprite_Enemy);break;
 		case 5: InicializarOvni(str_enemy, Block_sprite_Enemy);break;
-		case 6: break;
+		case 6: InicializarCruz(str_enemy, Block_sprite_Enemy);break;
 		case 7: InicializarAlcon(str_enemy, Block_sprite_Enemy);break;
 		case 8: InicializarSapo(str_enemy, Block_sprite_Enemy);break;
 		default: printf("ERROR AL INICIAR ENEMIGOS-g_level = %d", g_level);		
@@ -29,7 +29,7 @@ void ControlEnemyLevel(){
 		case 3: controlBola(); break;
 		case 4: controlAvion();break;
 		case 5: controlOvni();break;
-		case 6: break;
+		case 6: controlCruces();break;
 		case 7: DibujarAlcon();break;
 		case 8: controlSapo();break;
 		default: printf("ERROR AL CONTROLAR ENEMIGOS-g_level = %d", g_level);		
