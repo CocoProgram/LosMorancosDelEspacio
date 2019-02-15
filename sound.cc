5437cc10c22a39e87795dad01f5344aca1dbcc35
badwaz <alexcalabuig@gmail.com>
2019-02-15 23:46:44 +0100

    Update sound.cc

--- sound.cc
+++ sound.cc
@@ -7,18 +7,18 @@ void MemoriaAudio(){
 }
 
 void MenuBackgroundSound(){
-  canal.play(audio_intro,2.0f); //ponedlo luego a 20, que esto solo era trolling.
+  canal.play(audio_intro,1.0f); //ponedlo luego a 20, que esto solo era trolling.
 }
 
 
 void PlaySound(){
     if ( g_shoot == true) {
       audio_disparo.stop();
-      canal.play(audio_disparo ,10.0f);
+      canal.play(audio_disparo ,1.0f);
     }
 
   if (str_player.show_player == false) {
-    canal.play(audio_nave, 10.0f);
+    canal.play(audio_nave, 1.0f);
   } else {
   (audio_nave).stop();
   }
